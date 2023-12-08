28b38dc3b02f0263582504f4899660ce6067fcb7
Andrew Jaffray <andrewjaffray3@gmail.com>
2023-12-08 13:39:15 +0000

    vehicle movement

--- App.java
+++ App.java
@@ -1,11 +1,24 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class App {
 
+
+    private static LinkedList<Location> car;
+    private static final char taxiIcon = 'V';
+
+    public App() {
+
+        car = new LinkedList<>();
+
+    }
+
+
     public static void main(String[] args) {
 
         HashMap<String, Vehicle> listOfVehicles = new HashMap<String, Vehicle>();
+        TaxiLogic taxi = new TaxiLogic();
 
 
         listOfVehicles.put("14-D-1262", new Vehicle("14-D-1262", "Regular", "BMW"));
@@ -23,11 +36,47 @@ public class App {
         */
         Random rand = new Random();
 
-
+/*
         for (Vehicle vehicle : listOfVehicles.values()) {
             //System.out.println(vehicle.getIcon());
             map.placeOnMap(2,4,vehicle.getIcon());
         }
         map.printMap();
-    }
-}
+
+ */
+//map.placeOnMap(taxi.getVehicleLocation().getX(), taxi.getVehicleLocation().getY(), 'V');
+        // Location point = listOfVehicles.getFirst();
+
+        Location Newpoint = new Location(taxi.getVehicleLocation().getX(),taxi.getVehicleLocation().getY());
+        Direction direction = Direction.RIGHT;
+
+        //  for (Vehicle vehicle : listOfVehicles.values()) {
+        //System.out.println(vehicle.getIcon());
+        //map.placeOnMap(2, 4, vehicle.getIcon());
+
+        // }
+        for (int i=0; i <= 7; i++) {
+            // while (true) {
+            map.printMap();
+            System.out.println();
+            /*
+            if (taxi.getVehicleLocation().getX() >= 10)  {
+               direction = Direction.LEFT;
+            }
+            if (taxi.getVehicleLocation().getX() <= 0)  {
+                direction = Direction.RIGHT;
+            }
+            if (taxi.getVehicleLocation().getY() >= 10)  {
+                direction = Direction.UP;
+            }
+            if (taxi.getVehicleLocation().getY() <= 0)  {
+                direction = Direction.DOWN;
+            }
+             */
+
+            taxi.moveVehicle(direction, map);
+        }
+
+            }
+        }
+
